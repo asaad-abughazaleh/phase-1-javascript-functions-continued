6ec4e8ab7b43012d7ad25e5a810a2ea8ae18b15f
Asaad AbuGhazaleh <asaad.ag.98@gmail.com>
2022-10-02 01:27:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(satString = "roller-skate") {
+    return `This Saturday, I want to ${satString}!`
+}
+
+function mondayWork(monString = "go to the office") {
+    return `This Monday, I will ${monString}.`;
+}
+
+function wrapAdjective(symbol = "*") {
+    return function innerF(person = "a hard worker") {
+        return `You are ${symbol}${person}${symbol}!`;
+    }
+}
+wrapAdjective('||')('a dedicated programmer');
